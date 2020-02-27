01232c461d56954d1f568954d1e80e60652d5ccc
Hudson Chamberlain <hudson.m.chamberlain@gmail.com>
2020-02-27 03:27:47 -0600

    user settings dialog setup

--- dev-desk-queue/src/components/Dashboard/Dashboard.js
+++ dev-desk-queue/src/components/Dashboard/Dashboard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import SideNavigation from './SideNavigation';
+import UserSettingsDialog from '../User/UserSettingsDialog';
+
 
 const Dashboard = props => {
 
@@ -14,16 +16,21 @@ const Dashboard = props => {
         }
     ]);
 
-    const [currentLink, setCurrentLink] = React.useState({value: '/dashboard'})
+    const [open, setOpen] = React.useState(false);
 
     const onLinkClick = to => {
         // this function is the callback for when a link is clicked
         if(to === '/settings') {
             //popup the dialog for editing the user settings
+            handleClickOpen();
         } else {
             props.history.push('/ticket-manager');
         }
     }
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
     return (
         <div className='dashboard'>
@@ -31,6 +38,9 @@ const Dashboard = props => {
             <div className='user-dashboard'>
                 {/* determine what to render based on user type */}
                 User Dashboard
+            </div>
+            <div className='job-dialog-container'>
+                <UserSettingsDialog email='user@email.com' open={open} handleClose={() => setOpen(false)} onUpdateUserSettingsRequest={(info) => console.log(info)}/>
             </div>
         </div>
     )
---
+++ dev-desk-queue/src/components/User/UserSettingsDialog.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+
+const UserSettingsDialog = props => {
+
+    const [info, setInfo] = React.useState({
+        email: props.email,
+        password: '',
+        confirmPassword: ''
+    })
+
+    const handleChange = evt => {
+        setInfo({
+            ...info,
+            [evt.target.name]: evt.target.value
+        })
+    }
+
+    const handleUpdateRequest = () => {
+        // this function will validate the user input, if password is '', do not update
+        props.onUpdateUserSettingsRequest(info);
+        props.handleClose();
+    }
+
+    return (
+        <Dialog open={props.open} onClose={() => props.handleClose} aria-labelledby="form-dialog-title" fullWidth='false' maxWidth='sm'>
+            <DialogContent>
+                <h2>Update Your Settings</h2>
+                <h4>Enter details below:</h4>
+                <TextField 
+                    label='Email' 
+                    type='text' 
+                    name='email' 
+                    value={info.email} 
+                    onChange={(evt) => handleChange(evt)} />
+                <br />
+                <TextField 
+                    label='New Password' 
+                    type='password' 
+                    name='password' 
+                    value={info.password}
+                    onChange={(evt) => handleChange(evt)} />
+                <br />
+                <TextField 
+                    label='Confirm Password' 
+                    type='password' 
+                    name='confirmPassword' 
+                    value={info.confirmPassword}
+                    onChange={(evt) => handleChange(evt)} />
+            
+            </DialogContent>
+            
+            <DialogActions>
+
+                <Button onClick={() => handleUpdateRequest()} color="primary">
+                    Update
+                </Button>
+                <Button onClick={() => props.handleClose()} color="primary">
+                    Cancel
+                </Button>
+                
+            </DialogActions>
+
+        </Dialog>
+    )
+}
+
+export default UserSettingsDialog;
