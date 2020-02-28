662bc89e5139b0ca3b580a6dec75290540629091
adrianbparra <adrianplane12@gmail.com>
2020-02-27 17:53:39 -0700

    creating redux state

--- dev-desk-queue/src/index.js
+++ dev-desk-queue/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {Provider} from "react-router-dom";
+
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
