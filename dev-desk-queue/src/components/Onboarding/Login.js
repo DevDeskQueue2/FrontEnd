373f51b776b86752f6e484e36b2e80bcfec6bc44
Hudson Chamberlain <hudson.m.chamberlain@gmail.com>
2020-02-29 00:42:00 -0600

    base validation for login form

--- dev-desk-queue/src/components/Onboarding/Login.js
+++ dev-desk-queue/src/components/Onboarding/Login.js
@@ -9,18 +9,91 @@ const Login = props => {
         email: '', password: ''
     }
 
+    const initialErrors = {
+        email: {
+            isError: false,
+            helperText: ''
+        },
+        password: {
+            isError: false,
+            helperText: ''
+        }
+    }
+
+    const initialError = {
+        isError: false,
+        helperText: ''
+    }
+
     const [info, setInfo] = React.useState(initialInfo);
+    const [emailError, setEmailError] = React.useState(initialError)
+    const [passwordError, setPasswordError] = React.useState(initialError);
 
     const handleSubmit = evt => {
         evt.preventDefault();
-        props.onUserLoginRequest(info);
-        setInfo(initialInfo);
+
+        console.log('validate', validate());
+
+        if(validate()) {
+            props.onUserLoginRequest(info);
+            setInfo(initialInfo);
+        }
+
+        console.log('done with validation', emailError);
     }
+
+    const validate = () => {
+        //this functino validates user input
+        console.log('validating', info);
+        let isNoError = true;
+        let error = {
+            isError: false,
+            helperText: ''
+        }
+
+        let newPasswordError =  {
+            isError: false,
+            helperText: ''
+        }
+
+        if(!validateEmail(info.email)) {
+            error.isError = true;
+            error.helperText = 'Invalid Address';
+            isNoError = false;
+        }
+
+        setEmailError(error);
+
+        if(!validatePassword(info.password)) {
+            newPasswordError.isError = true;
+            newPasswordError.helperText = 'Invalid Password'
+            isNoError = false;
+        }
+
+        setPasswordError(newPasswordError);
+
+        function validateEmail(elementValue){      
+            var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+            return emailPattern.test(elementValue); 
+          } 
+
+        function validatePassword(password) {
+            //To check a password between 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter
+            var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+            return re.test(String(password));
+        }
+
+        return isNoError;
+    }
+
+
 
     return (
         <div className='onboarding-form login-form'>
             <FormGroup>
                 <TextField 
+                    error={emailError.isError}
+                    helperText={emailError.helperText}
                     placeholder='Email' 
                     type='text' 
                     name='email' 
@@ -30,6 +103,8 @@ const Login = props => {
                 <br />
 
                 <TextField 
+                    error={passwordError.isError}
+                    helperText={passwordError.helperText}
                     placeholder='Password' 
                     type='password' 
                     name='password' 
@@ -37,6 +112,7 @@ const Login = props => {
                     onChange={ (evt) => setInfo({ ...info, [evt.target.name]: evt.target.value}) } />
 
                 <br />
+
             </FormGroup>
             <div className='onboarding-nav-buttons'>
                 <Button variant='outlined' onClick={(evt) => handleSubmit(evt)}>Login</Button> 
